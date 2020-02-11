9388fb77533cdb75f83e22f8c2434c9cdaf22fbc
Arpit Mathur <arpitmathur19@gmail.com>
2020-02-11 00:25:32 -0500

    animations

---
+++ js/typed.js
@@ -0,0 +1,14 @@
+    var txt = "Namaste, I'm Arpit";
+    var i = txt.length;
+    var speed = 50;
+    var temp = "";
+
+    function typeWriter() {
+        temp += txt.charAt(i);
+        if (i >= 0) {
+            document.getElementById("demo").innerHTML = temp.split("").reverse().join("");
+            i--;
+            setTimeout(typeWriter, speed);
+        }
+    }
+    window.onload = setTimeout(typeWriter, 750);
